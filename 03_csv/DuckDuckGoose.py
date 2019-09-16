cd0146dbd0bffadc00ec12ad0b8b2fe9746b72d7
Brandon Chen <bchen00@stuy.edu>
2019-09-15 21:10:29 -0400

    finished assignment and it should work

--- 03_csv/DuckDuckGoose.py
+++ 03_csv/DuckDuckGoose.py
@@ -1,1 +1,19 @@
-
+import csv
+import random
+def randomOccupation():
+    with open('occupations.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        linenum = 0
+        allOccupation = {}
+        for rows in reader:
+            if linenum != 0:
+                allOccupation[rows[0]] = float(rows[1])
+            linenum += 1
+        counter = 0
+        randomnum = random.uniform(0,allOccupation.get("Total"))
+        for key, value in allOccupation.items():
+            counter += value
+            if randomnum <= counter:
+                return key
+                                  
+print(randomOccupation())
