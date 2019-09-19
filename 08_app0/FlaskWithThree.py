b6e30afd74dceafbb0b68a62fbe1c19bb4c14327
Brandon Chen <bchen00@stuy.edu>
2019-09-18 20:48:10 -0400

    created three routes

--- 08_app0/FlaskWithThree.py
+++ 08_app0/FlaskWithThree.py
@@ -1,1 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
 
+
+@app.route("/first")
+def hello_world():
+    print(__name__)
+    return "First route here."
+
+@app.route("/second")
+def bye_world():
+    print(__name__)
+    return "Second route here."
+
+@app.route("/third")
+def hello_again_world():
+    print (__name__)
+    return "Third route here."
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
