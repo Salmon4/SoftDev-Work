3b487cf2568b2c4e38124c5f09ad4132bbc94c60
Brandon Chen <bchen00@stuy.edu>
2019-10-09 21:44:24 -0400

    was able to add all values from both students and courses to sqlite3 table but needs some cleanup and the header

---
+++ 17_csv2db/db_builder.py
@@ -0,0 +1,47 @@
+#Clyde "Thluffy" Sinclair
+#SoftDev
+#skeleton :: SQLITE3 BASICS
+#Oct 2019
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+COURSE_FILE = "courses.csv";
+STUDENT_FILE = "students.csv";
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+
+# < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+with open(STUDENT_FILE, newline='') as csvfile:
+     reader = csv.DictReader(csvfile)
+     test = "CREATE TABLE students (name TEXT,age INTEGER, id INTEGER);"
+     c.execute(test)
+     for row in reader:
+         #test = "INSERT INTO students VALUES (?, ?, ?)"
+         #+ str(row["name"])+"," + str(row["age"]) + "," + str(row["id"],) + ");"
+         c.execute("INSERT INTO students VALUES (?, ?, ?)", (row["name"], row["age"], row["id"]))
+
+c.execute("SELECT * FROM students;")
+
+with open(COURSE_FILE, newline='') as csvfile:
+      reader = csv.DictReader(csvfile)
+      #headers = next(reader)
+      #print(headers)
+      test = "CREATE TABLE courses (code TEXT,mark INTEGER, id INTEGER);"
+      c.execute(test)
+      for row in reader:
+          #test = "INSERT INTO courses VALUES ("+ str(row["code"], ) + str(row["mark"], ) + str(row["id"],) + ");"
+          c.execute("INSERT INTO courses VALUES (?, ?, ?)", (row["code"], row["mark"], row["id"]))
+        #print(row)
+c.execute("SELECT * FROM courses;")
+#c.execute(command)
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database#Clyde "Thluffy" Sinclair
