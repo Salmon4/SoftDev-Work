415bfda15cda926f582c2ff83144fb4d96006ee6
Brandon Chen <bchen00@stuy.edu>
2019-10-12 22:26:20 -0400

    was able to create the table

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -17,6 +17,7 @@ c = db.cursor()               #facilitate db ops
 
 c.execute("DROP TABLE IF EXISTS courses")
 c.execute("DROP TABLE IF EXISTS students")
+c.execute("DROP TABLE IF EXISTS stu_avg")
 
 with open(STUDENT_FILE, newline='') as csvfile:
      reader = csv.DictReader(csvfile)
@@ -33,18 +34,13 @@ with open(COURSE_FILE, newline='') as csvfile:
           c.execute("INSERT INTO courses VALUES (?, ?, ?)", (row["code"], row["mark"], row["id"]))
 
 def getStudentID(name):
-     cur = c.execute("SELECT id FROM students WHERE name = ?", [str(name)])
-     return cur.fetchone()[0]
+    cur = c.execute("SELECT id FROM students WHERE name = ?", [str(name)])
+    return cur.fetchone()[0]
 
 #print(getStudentID("armin"))
 
 def getStudentGrade(name):
     cur = c.execute("SELECT mark FROM courses WHERE id = ?", [getStudentID(name)])
-    allGrades = cur.fetchall()
-    return allGrades
-
-def getStudentGradeID(id):
-    cur = c.execute("SELECT mark FROM courses WHERE id = ?", [id])
     allGrades = cur.fetchall()
     return allGrades
 
@@ -54,21 +50,16 @@ def getAverage(name):
     allGrades = getStudentGrade(name)
     average = 0
     length = 0
-    for row in allGrades:
-        average += row[0]
+
+    for row1 in allGrades:
+        row = row1[0]
+        average += row
         length += 1;
     return average / length;
 
 #print(getAverage("armin"))
 
-def getAverage(id):
-    allGrades = getStudentGradeID(id)
-    average = 0
-    length = 0
-    for row in allGrades:
-        average += row[0]
-        length += 1;
-    return average / length;
+
 
 def displayStudent(name):
     return "name: " + name + " id: " + str(getStudentID(name)) + " average: " + str(getAverage(name))
@@ -80,8 +71,10 @@ def displayStudent(name):
 c.execute("CREATE TABLE stu_avg (id INTEGER, avg INTEGER);")
 cur = c.execute("SELECT name FROM students")
 allNames = cur.fetchall()
-for name in allNames:
-    c.execute("INSERT INTO stu_avg VALUES (?, ?)", (str(getStudentID(name)), str(getAverage(name))))
+for name1 in allNames:
+    name = name1[0]
+    command = "INSERT INTO stu_avg VALUES (" + str(getStudentID(name)) + ", " + str(getAverage(name)) + ")"
+    c.execute(command)
 
 db.commit() #save changes
 db.close()  #close database
