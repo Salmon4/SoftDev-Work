94c280a10d622e8032106c3c8a216c569ff41e35
Brandon Chen <bchen00@stuy.edu>
2019-11-12 18:58:02 -0500

    finished:

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template
-import urllib2, json
+from urllib.request import urlopen
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    u = urllib2.urlopen(
+    u = urlopen(
     "https://api.nasa.gov/planetary/apod?api_key=jbSyCSZme7kPo6nqEwmbELOLAVP64FqdcEQQ4Cww"
     )
     response = u.read()
     data = json.loads( response )
-    return render_template("index.html", pic = data['url'])
+    return render_template("index.html",t = data['title'], day = data['date'], c = data['url'], pic = data['url'], explain = data['explanation'])
 
 if __name__ == "__main__":
     app.debug = True
