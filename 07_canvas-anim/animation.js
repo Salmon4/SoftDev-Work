c8ac36a31a60d41e7f374e6260c3391f2e832fd1
Brandon Chen <bchen00@stuy.edu>
2020-02-12 22:23:05 -0500

    finished

--- 07_canvas-anim/animation.js
+++ 07_canvas-anim/animation.js
@@ -1,29 +1,41 @@
 var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
-var start = false;
 var radius = 1;
+var id = 0;
+var direction = 1;
 
 var draw = function(e) {
-    var x = 300;//top left corner xcoord of mouse //xcoord of mouse pointer relative to target element
-    var y = 300;//top left corner ycoord of mouse //ycoord of mouse pointer relative to target element
+    var x = 300;
+    var y = 300;
+    clear();
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.fill();
-    ctx.stroke()
-
-    radius += 1;
-    window.requestAnimationFrame(draw);
+    ctx.stroke();
+    if (radius >= 300 || radius <= 0){
+      direction *= -1;
+    }
+    radius += 1 * direction;
+    id = window.requestAnimationFrame(draw);
 }
 
-window.requestAnimationFrame(draw);
+var animate = function(e){
+  if (id == 0){
+    draw();
+  }
+}
 
 var clear = function(e) {
     ctx.clearRect(0, 0, 600, 600)
 }
 
+var stop = function(e) {
+    window.cancelAnimationFrame(id);
+    id = 0;
+}
 
 var clearButton = document.getElementById("stop");
 clearButton.addEventListener("click", stop);
 
 var animateButton = document.getElementById("animate");
-clearButton.addEventListener("click", draw);
+animateButton.addEventListener("click", animate);
