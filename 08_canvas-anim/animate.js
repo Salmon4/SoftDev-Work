a7c5ddf518d5936f3e1d0be48ae62268483ab832
Brandon Chen <bchen00@stuy.edu>
2020-02-13 21:37:27 -0500

    yeaaaaaaaaaaaaaaaaaaaaaaa

---
+++ 08_canvas-anim/animate.js
@@ -0,0 +1,84 @@
+/*
+Tyler Huang
+SoftDev Pd. 2
+K #08: What is it saving the screen from?
+2020-02-20
+*/
+
+var c = document.getElementById("slate");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#6a0dad";
+var circleId, dvdId, dvdX, dvdY;
+var radius = 10;
+var growth = 1;
+var dirX = 1;
+var dirY = 1;
+var logo = new Image();
+logo.src = "logo_dvd.jpg";
+
+var clear = function(e){
+	e.preventDefault();
+	ctx.clearRect(0,0,c.width,c.height);
+}
+
+var growCircle = function(){
+	window.cancelAnimationFrame(circleId);
+	window.cancelAnimationFrame(dvdId);
+	ctx.clearRect(0,0,c.width,c.height);
+	var x = c.width / 2;
+	var y = c.height / 2;
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, 2 * Math.PI);
+	ctx.stroke();
+	ctx.fill();
+	if (radius == c.width / 2){
+		growth = -1;
+		//console.log(growth);
+	}
+	else if (radius == 0){
+		growth = 1;
+	}
+	radius += growth;
+	//console.log(radius);
+	circleId = window.requestAnimationFrame(growCircle);
+}
+
+var displayDVD = function(){
+	ctx.clearRect(0,0,c.width,c.height);
+	dvdX = Math.round(Math.random() * (c.width - 250) + 125);
+	dvdY = Math.round(Math.random() * (c.height - 250) + 125);
+	ctx.drawImage(logo,dvdX,dvdY,50,50);
+}
+
+var animateDVD = function(){
+	window.cancelAnimationFrame(circleId);
+	window.cancelAnimationFrame(dvdId);
+	ctx.clearRect(0,0,c.width,c.height);
+	dvdX += dirX;
+	dvdY += dirY;
+	//console.log(dvdX);
+	if (dvdX + 50 == c.width || dvdX + 2 == 0){
+		dirX = -1 * dirX;
+		//console.log(dvdX);
+	}
+	if (dvdY + 43 == c.height || dvdY + 7 == 0){
+		dirY = -1 * dirY;
+		//console.log(dvdY);
+	}
+	ctx.drawImage(logo,dvdX,dvdY,50,50);
+	dvdId = window.requestAnimationFrame(animateDVD);
+}
+
+var stop = function(){
+	window.cancelAnimationFrame(circleId);
+	window.cancelAnimationFrame(dvdId);
+}
+
+var stopButton = document.getElementById("stop");
+stopButton.addEventListener("click", stop);
+
+var circle = document.getElementById("animate");
+circle.addEventListener("click", growCircle);
+
+var dvd = document.getElementById("waiting");
+dvd.addEventListener("click", function(e){displayDVD(); animateDVD()});
