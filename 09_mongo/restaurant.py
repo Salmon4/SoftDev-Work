670acf8ddc6ec0c7fc4a7d49506acc7488b3791b
Brandon <bchen00@stuy.edu>
2020-02-27 14:22:46 +0000

    work

--- 09_mongo/restaurant.py
+++ 09_mongo/restaurant.py
@@ -1,5 +1,12 @@
-from pyongo import MongoClient
+import json
+from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017);
 db = client.K09
 collection = db.restaurants
+
+with open('primer-dataset.json') as f:
+    file_data = json.load(f)
+
+collection.insert_one(file_data)
+client.close()
