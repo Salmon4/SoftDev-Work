6e1fcaf2b7852f4f589c4314d5bb21f65ae0327c
Brandon <bchen00@stuy.edu>
2020-03-05 09:23:19 -0500

    added code from partner

--- 10_mongo/meteor.py
+++ 10_mongo/meteor.py
@@ -46,3 +46,35 @@ def findYear(name):
         print(result["year"])
 
 findYear("Aachen")
+
+def findMassbyID(id):
+    id = str(id)
+    results = collection.find({"id" : id})
+    for result in results:
+        print(result["mass"])
+findMassbyID(2)
+
+
+def findLocationbyID(id):
+    id = str(id)
+    results = collection.find({"id": id})
+    for result in results:
+        ans = result["reclat"] + " " + result["reclong"]
+        print(ans)
+findLocationbyID(2)
+
+
+def findClassbyID(id):
+    id = str(id)
+    results = collection.find({"id" : id})
+    for result in results:
+        print(result["recclass"])
+findClassbyID(2)
+
+
+def findYearbyID(id):
+    id = str(id)
+    results = collection.find({"id" : id})
+    for result in results:
+        print(result["year"])
+findYearbyID(2)
