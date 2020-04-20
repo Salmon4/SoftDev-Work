f3c743d7db9dba18d339201a08131337d1702091
Brandon Chen <bchen00@stuy.edu>
2020-04-20 12:04:10 -0400

    got graph axis to appear but need to incorporate it into render button and add line

---
+++ 18_d3/static/js/script.js
@@ -0,0 +1,30 @@
+var margin = {top: 10, right: 30, bottom: 30, left: 100},
+    width = 1000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+var svg = d3.select("#chart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+d3.json("/static/data/population.json").then(function(data) {
+  data = data[1]
+  console.log(data)
+
+  var x = d3.scaleTime()
+    .domain(d3.extent(data, function(d) { return d.date; }))
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  var y = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return +d.value; })])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+})
